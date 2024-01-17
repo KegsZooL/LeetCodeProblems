bf793e1636a5113282698c16f215a2fdc8e0bd3c
KegsZooL <kegs.zool.2015@inbox.ru>
2024-01-17 16:49:09 +0300

    added new solution

--- src/com/leetcode/trees/problem_108/Solution.java
+++ src/com/leetcode/trees/problem_108/Solution.java
@@ -13,17 +13,17 @@ public class Solution {
         return sortedArrayToBST(nums, 0, nums.length - 1);
     }
 
-    private TreeNode sortedArrayToBST(int[] nums, int left, int right){
+    private TreeNode sortedArrayToBST(int[] nums, int start, int end){
 
-        if(left > right){
+        if(start > end){
             return null;
         }
 
-        int middle = left + (right - left) / 2;
+        int middle = start + (end - start) / 2;
         TreeNode root = new TreeNode(nums[middle]);
 
-        root.left = sortedArrayToBST(nums, left, middle - 1);
-        root.right = sortedArrayToBST(nums, middle + 1, right);
+        root.left = sortedArrayToBST(nums, start, middle - 1);
+        root.right = sortedArrayToBST(nums, middle + 1, end);
 
         return root;
     }
